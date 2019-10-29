07b41a15e20ae15ef7051e746f13aee316ad6b75
Xin Wang <50649@icf.com>
2019-10-29 13:02:47 -0400

    I have no idea what the song folder is about.

---
+++ src/components/songDetail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const SongDetail = ({ song }) => {
+    //console.log(song);
+    if(!song) {
+        return <div>Please Select A Song!</div>
+    }
+    return (
+        <div>
+            <h3>Details for:</h3>
+            <p>
+                Title: {song.title}
+                <br /> 
+                Duration: {song.duration}
+            </p>
+        </div>
+    );
+}
+
+const mapStateToProps = (state) => {
+    return { song: state.selectedSong }
+}
+
+export default connect(mapStateToProps)(SongDetail);
---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+    return [
+        { title: 'No Scrubs', duration: '4:05' },
+        { title: 'All Star', duration: '2:03' },
+        { title: 'Macarena', duration: '3:04' },
+        { title: 'Uppertown Funk', duration: '3:21' }
+    ];
+};
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+}
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
