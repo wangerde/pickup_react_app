9d3003152e790a22e6b933bf7c36e0f4e2fea5e8
Xin Wang <50649@icf.com>
2019-10-29 15:21:11 -0400

    被人打断了 先停在这儿 still tic tac

--- src/tictac/board.js
+++ src/tictac/board.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Square from './square';
 import './tictac.css';
 
--- src/tictac/square.js
+++ src/tictac/square.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 class Square extends React.Component {
     constructor(props) {
@@ -6,12 +6,13 @@ class Square extends React.Component {
       this.state = {
         value: null,
       }
+      console.log(this);
     }
     render() {
       return (
         <button 
           className="square" 
-          onClick={() => this.setState({value: 'X'})}
+          onClick={() => {this.setState({value: 'X'});console.log(this)}}
         >
           {this.state.value}
         </button>
